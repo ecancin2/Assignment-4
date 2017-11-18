bb5a3dc0c053e686f072fdd5350facd4943534a4
Eric Cancino <eric.cancino01@utrgv.edu>
2017-11-17 19:41:34 -0600

    Changed to php

---
+++ index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>The Breed Scanner</title>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="Assign4.css" rel="stylesheet">
+  </head>
+
+  <body id="bg">
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="https://getbootstrap.com/docs/4.0/components/navbar/">Navbar</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="Assign4.html">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="http://www.google.com">Google Link</a>
+          </li>
+		  <!--
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+		  -->
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="dropdown01" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#id1">Current Pet Owners</a>
+              <a class="dropdown-item" href="#id2">New Pet Owners</a>
+              <a class="dropdown-item" href="#id3">Curious People</a>
+			  <div class="dropdown-divider"></div>
+			  <a class="dropdown-item" href="#id4">Solution</a>
+			  <a class="dropdown-item" href="#id5">Pricing</a>
+            </div>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
+    </nav>
+
+    <div class="container">
+
+      <div class="starter-template">
+	  
+        <h1 class="header1"><strong>The Breed Scanner</strong></h1>
+		
+		<h2 class="header2"><strong>An App to Scan your Pet's Breed!</strong></h2>
+		
+		<img class="border border-warning rounded" src="pet scan pic.jpg" style="width:300px;heigth:250px;"/>
+		
+		<br>
+		<br>
+		
+        <p class="border border-danger rounded lead">
+			<strong>Welcome to the breed scanner website! Here, we will show 
+			you what our wonderful app can do! Ever wondered what breed your 
+			pet or a stray cat or dog was out of sheer curiosity? Using our 
+			trusty pet scanner, just scan your pet's face, and bingo, that's 
+			all the steps you need! In addition, you get facts and ways to improve
+			your pet's lifestyle! Be in awe at the extreme convenience this 
+			app can hold!<br> </strong>
+		</p>
+		
+		
+		
+		<a href="http://www.theuselessweb.com" class="btn btn-success" role="button" aria-pressed="true">Buy Now!</a>
+		
+		<br>
+		<br>
+		
+		<div class="row" >
+			<div class="border border-warning rounded col-4" id="id1">
+			
+				<h3>Current Pet Owners<br></h3>
+				
+				<p><strong>
+					If you currently own a pet, you may want to be informed on the type of breed your pet is. By scanning
+					your pet and getting information on your breed, you can find out the right type of dog food for them 
+					that are recommended by veternarians for puppies/kittens or adults. In addition, you can find how 
+					much they should eat depending on their age, how much should they be walked if they're dog, how long 
+					they should be indoor or outdoor if they're a cat, and what type of vaccinations each breed should get.
+					</strong>
+				<br>
+				</p>
+				
+				<a href="http://www.baconsizzling.com" class="btn btn-success" role="button" aria-pressed="true">Buy It Now!</a>
+				
+			</div>
+		
+			<div class="border border-warning rounded col-4" id="id2">
+			
+				<h3>New Pet Owners<br></h3>
+				
+				<p><strong>
+					If you are interseted in adopting a new pet, whether a cat or a dog, you may want to know the details of 
+					the breed of each pet that you are highly considering in getting. Of course, an alternative method can be to use Google
+					instead, and write down the info there is. However, with the app, you don't need to remember what you googled.
+					You can simply go to the pet store, scan any dog or cat you identify as the ones you are interested in, and get all
+					the info you need about them when making an informed decision. You can know about their personalities, whether they're 
+					indoor or outdoor, etc.
+					</strong>
+				<br>
+				</p>
+				
+				<a href="http://www.trypap.com" class="btn btn-success" role="button" aria-pressed="true">Buy It Now!</a>
+
+			</div>
+		
+			<div class="border border-warning rounded col-4" id="id3">
+			
+				<h3>Curious People<br></h3>
+				
+				<p><strong>
+					If you are very curious about stray dogs or cats that are around your neighborhood, or curious when visiting friends or 
+					family who happen to have pets with them, you may want to satiate that curiosity. Instead of trying to Google the breed
+					that you think is correct from a simple observation, with our app, you can get the result you desire at the press of a 
+					button! Just press to scan the pet's face, and now you have solved the mystery of the pet's breed!
+				<br>
+				</p>
+				
+				<a href="http://www.randomcolour.com" class="btn btn-success" role="button" aria-pressed="true">Try It Out!</a>
+
+			</div>
+			
+		</div>
+		
+		<br>
+		
+		<div class="row">
+		
+		<div class="col-6" id="rotateimg">
+
+			<img class="border border-success rounded" src="catsanddogs2.jpg" style="width:275px;heigth:225px;"/>
+
+		</div>
+
+		<div class ="col-6" id="rotateimg2">
+
+			<img class="border border-success rounded" src="catsanddogs3.jpg" style="width:300px;heigth:250px;"/>
+
+		</div>
+		
+		</div>
+		
+		<br>
+		
+		<div class="row">
+		
+			<div class="border border-success rounded col-6" id="id4">
+				<h3>Solution</h3>
+				<p><strong>
+					The app would be a scanning device where you scan the face of your pet or someone else's pet, as well as a stray, 
+					and would show you the correct breed based on their facial features. There would be a database storing info for
+					every cat and dog breed possible, guaranteeing the right breed for your pet.
+					</strong>
+				<br>
+				</p>
+				
+				<a href="http://www.chrismckenzie.com" class="btn btn-success" role="button" aria-pressed="true">Buy It Now!</a>
+			</div>
+		
+			<div class="border border-primary rounded col-6" id="id5">
+				<h3>Pricing</h3>
+				<p><strong>
+					This app would be a one-time purchase on the app store, and the price would be around $1.99, or $2.99 at the 
+					maximum. It would be reasonably cheap to get, an would grant you unlimited scanning power!
+				   </strong>
+				<br>
+				</p>
+				
+				<a href="http://www.hasthelargehadroncolliderdestroyedtheworldyet.com" class="btn btn-success" role="button" aria-pressed="true">Buy It Now!</a>
+			</div>
+			
+		</div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
+
